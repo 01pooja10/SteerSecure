1a0d603b6609e481886d46e72e30b70f0ab0e167
Aditya Shukla <adityashukzy@gmail.com>
2020-11-21 10:03:41 +0530

    Update file structure and add drowsiness functionality
    Added drowsiness function in streamlit app and uploaded alarm and landmarks

--- st_app.py
+++ st_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
-import keras
+from tensorflow import keras
 import cv2
+import numpy as np
+
+list1 = ['Adjusting hair or makeup', 'Drinking a beverage', 'Operating the radio', 'Reaching behind', 'Safe driving', 'Talking on the phone(left)', 'Talking on the phone(right)', 'Talking to a passenger', 'Texting using phone(left)', 'Texting using phone(right)']
 
 def load_model(path):
 	model = keras.models.load_model(path)
@@ -15,8 +18,8 @@ def realTime(model):
 	while True:
 		ret, frame = capture.read()
 		# img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-		img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-		img = cv2.resize(img, (224,224))
+		img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+		img = cv2.resize(img, (224,224))/255
 
 		real_time.append(img)
 		x = real_time
@@ -30,17 +33,19 @@ def realTime(model):
 			pred = model.predict(mm)
 			kk = np.argmax(pred)
 
-			cv2.putText(frame, list1[kk], (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
-			# cv2.imshow('frame', frame)
-			# placeHolder.image(frame)
-			# figure out how to have cv2 close with esc key
+		cv2.putText(frame, list1[kk], (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+		# cv2.imshow('frame', frame)
+		placeHolder.image(frame, use_column_width=True, channels='RGB')
+		# figure out how to have cv2 close with esc key
 
-			key = cv2.waitKey(1)
+		key = cv2.waitKey(1)
 			
-			# cv2.waitKey not working
-			if key == 27:
-				break
-		
+		# cv2.waitKey not working
+		if key == 27:
+			break
+	
+	# webcam not stopping even after capture.release()
+	# maybe try st.stop()
 	capture.release()
 	cv2.destroyAllWindows()
 	return real_time
@@ -60,8 +65,132 @@ def distractionDet(model):
 	if st.button("Launch Application"):
 		realTime(model)
 
+# Drowsiness
+def EAR(landmarks, frame):
+	left = []
+	right = []
+	alarm = 0
+
+	# left eye
+	for i in range(36,42):
+		x = landmarks.part(i).x
+		y = landmarks.part(i).y
+		left.append((x,y))
+
+		nextpt1 = i + 1
+		if i == 41:
+			nextpt1 = 36
+
+		x1 = landmarks.part(nextpt1).x
+		y1 = landmarks.part(nextpt1).y
+
+	# right eye
+	for i in range(42,48):
+		x = landmarks.part(i).x
+		y = landmarks.part(i).y
+		right.append((x,y))
+
+		nextpt2 = i + 1
+		if i == 47:
+			nextpt2 = 42
+
+		x1 = landmarks.part(nextpt2).x
+		y1 = landmarks.part(nextpt2).y
+
+	# calculate ratio
+	left_EAR = aspect_ratio(left)
+	right_EAR = aspect_ratio(right)
+	ratio = ((left_EAR + right_EAR)/2)
+	return ratio
+
+def aspect_ratio(eye):
+	eye = np.squeeze(eye)
+	a = np.linalg.norm(eye[1] - eye[5])
+	b = np.linalg.norm(eye[2] - eye[4])
+	c = np.linalg.norm(eye[0] - eye[3])
+	
+	return (a + b)/(2.0 * c)
+
+def yawn(landmarks, frame):
+	top, bottom = [], []
+	
+	for i in range(51,54): # should rather be 51, 54
+		x = landmarks.part(i).x
+	
+	for i in range(62,65): # should rather be 62, 65
+		y = landmarks.part(i).y
+	
+	top.append((x,y))
+    
+	for i in range(66,69): # should rather be 66, 69
+		x1 = landmarks.part(i).x
+
+	for i in range(57,60): # should rather be 57, 60
+		y1 = landmarks.part(i).y
+
+	bottom.append((x1,y1))
+
+	top_mean = np.mean(top, axis=0)
+	bottom_mean = np.mean(bottom, axis=0)
+
+	dist = abs(top_mean[1] - bottom_mean[1])
+	return dist
+
+def detect():
+	capture = cv2.VideoCapture(0)
+
+	detect = dlib.get_frontal_face_detector()
+	predict = dlib.shape_predictor(r"file dependencies/shape_predictor_68_face_landmarks.dat")
+	c = 0
+	alarm = 0
+	placeHolder2 = st.empty()
+	
+	while True:
+		ret, frame = capture.read()
+		gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+		faces = detect(gray)
+		
+		for face in faces:
+			p = face.left()
+			q = face.top()
+			r = face.right()
+			s = face.bottom()
+			landmarks = predict(gray, face)
+			
+			for i in range(68):
+				x = landmarks.part(i).x
+				y = landmarks.part(i).y
+
+			yawn_threshold = 22
+			eye_closed = 0.26
+			eye_threshold = 47
+			eye_min = 30
+
+			x = EAR(landmarks, frame)
+			if x < eye_closed:
+				c += 1
+				if c >= eye_threshold:
+					playsound(r"file dependencies/alarm.mp3")
+					c = 0
+
+			y = yawn(landmarks, frame)
+			if y > yawn_threshold:
+				cv2.putText(frame,"YOU SEEM DROWSY!", (10,30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+			
+			placeHolder2.image(frame, caption="Driver Live Video", use_column_width=True)
+		
+		key = cv2.waitKey(1)
+		if key == 27:
+			break
+	
+	capture.release()
+	cv2.destroyAllWindows()
+
 def drowsinessDet():
-	pass
+	st.title("Drowsiness Detector")
+	
+	if st.button("Launch Application"):
+		detect()
 
 def main():
 	link = 'https://images.unsplash.com/photo-1519865241348-e0d7cd33a287?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
@@ -77,7 +206,7 @@ def main():
 	st.markdown("")
 
 	if option == 'Distraction Detector':
-		model = load_model('vgg_model.h5')
+		model = load_model('models/vgg_model.h5')
 		distractionDet(model)
 
 	elif option == 'Drowsiness Detector':
